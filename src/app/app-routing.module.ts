798accfebf5fb4bbf04df1822e00c40644a5060d
FelipeCollantes <felipe.pcollantes@gmail.com>
2023-09-17 17:11:02 +0200

    Add DataModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DataModule } from './data/data.module';
 
 const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule, DataModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
