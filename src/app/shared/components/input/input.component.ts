b6bf8e98bae63ff3b43fecd5249fb2abb13adcc8
Felipecollantes <felipe.pcollantes@gmail.com>
2023-09-17 21:22:40 +0200

    Create input

--- src/app/shared/components/input/input.component.ts
+++ src/app/shared/components/input/input.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { debounceTime, distinctUntilChanged } from 'rxjs';
 
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
-  styleUrls: ['./input.component.scss']
+  styleUrls: ['./input.component.scss'],
 })
 export class InputComponent {
+  @Input() controlName: string = '';
+  @Input() form: FormGroup;
+  @Output() param = new EventEmitter<string>();
 
+  constructor() {
+    this.form = new FormGroup({
+      search: new FormControl(''),
+    });
+  }
+  ngOnInit(): void {
+    this.form.controls['search']?.valueChanges.pipe(debounceTime(500), distinctUntilChanged()).subscribe((response) => {
+      this.param.emit(response);
+    });
+  }
 }
