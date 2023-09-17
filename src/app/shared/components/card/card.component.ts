a3f87125a0e3c6e1c953de004dec7b359f0e63f4
Felipecollantes <felipe.pcollantes@gmail.com>
2023-09-17 21:18:33 +0200

    Card htm and css

--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ProductModel } from 'src/app/domain/product/models/product.model';
 
 @Component({
-  selector: 'app-card',
+  selector: 'custom-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent {
-
+  @Input() product = {} as ProductModel;
 }
