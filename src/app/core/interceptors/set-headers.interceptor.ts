499cff186c2357d1a43f55592e20dc2fdb693d93
FelipeCollantes <felipe.pcollantes@gmail.com>
2023-09-18 16:45:34 +0200

    Implement interceptor

--- src/app/core/interceptors/set-headers.interceptor.ts
+++ src/app/core/interceptors/set-headers.interceptor.ts
@@ -1,13 +1,27 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Store } from '@ngrx/store';
+import { Observable, catchError, throwError } from 'rxjs';
+import { RootState } from 'src/app/data/store';
+import * as FromUser from '../../data/store/user/selectors';
+import { Injectable } from '@angular/core';
 
-// export class SetHeaderInterceptor implements HttpInterceptor {
-//   constuctor() {}
+@Injectable({
+  providedIn: 'root',
+})
+export class SetHeaderInterceptor implements HttpInterceptor {
+  private accessToken: string | undefined = '';
+  constructor(private store: Store<RootState>) {
+    this.store.select(FromUser.selectToken).subscribe((accessToken) => (this.accessToken = accessToken?.access_token));
+  }
 
-//   //   intercept<T>(req: HttpRequest<T>, next: HttpHandler): Observable<HttpEvent<any>> {
-//   //     // req = req.clone({
-//   //     //   setHeaders: { Authorization: this.getInteractionPoint() },
-//   //     // });
-//   //     return next.handle(req);
-//   //   }
-// }
+  intercept<T>(req: HttpRequest<T>, next: HttpHandler): Observable<HttpEvent<any>> {
+    if (this.accessToken) req = req.clone({ setHeaders: { authorization: `Bearer ${this.accessToken}` } });
+    return next.handle(req).pipe(
+      catchError((error: HttpErrorResponse) => {
+        if (error.status >= 500) console.error(error);
+        if (error.status === 401) console.error('Unauthorized ', error);
+        return throwError(() => error);
+      })
+    );
+  }
+}
