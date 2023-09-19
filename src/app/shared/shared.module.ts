c86f18946bac0df830ac07b2fd1681f61e2f0c04
FelipeCollantes <felipe.pcollantes@gmail.com>
2023-09-19 16:03:30 +0200

    Create button

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,20 +4,12 @@ import { CardComponent } from './components/card/card.component';
 import { InputComponent } from './components/input/input.component';
 import { LoadingComponent } from './components/loading/loading.component';
 import { ReactiveFormsModule } from '@angular/forms';
-
-
+import { ButtonComponent } from './components/button/button.component';
+import { ScrollingModule } from '@angular/cdk/scrolling';
 
 @NgModule({
-  declarations: [
-    CardComponent,
-    InputComponent,
-    LoadingComponent
-  ],
-  imports: [
-    CommonModule,
-    ReactiveFormsModule
-  ],
-  exports: [CardComponent, InputComponent, LoadingComponent, ReactiveFormsModule],
-
+  declarations: [CardComponent, InputComponent, LoadingComponent, ButtonComponent],
+  imports: [CommonModule, ReactiveFormsModule, ScrollingModule],
+  exports: [CardComponent, InputComponent, LoadingComponent, ButtonComponent, ReactiveFormsModule, ScrollingModule],
 })
-export class SharedModule { }
+export class SharedModule {}
