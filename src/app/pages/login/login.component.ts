d5452d56c9128bf06527c59d68d1841340b83879
Felipecollantes <felipe.pcollantes@gmail.com>
2023-09-20 09:28:34 +0200

    Implement login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,15 +1,20 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { RootState } from 'src/app/data/store';
 import * as UserActions from '../../data/store/user/actions';
+import * as FromUser from '../../data/store/user/selectors';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LoginComponent {
+  public user$ = this.store.select(FromUser.selectUser);
+  public loading$: Observable<boolean> = this.store.select(FromUser.selectLoading);
   form: FormGroup;
 
   constructor(private fb: FormBuilder, private store: Store<RootState>) {
@@ -19,14 +24,17 @@ export class LoginComponent {
     });
   }
 
-  ngOnInit(): void {}
+
 
   onSubmit() {
-    console.log('Form', this.form.value);
     if (this.form.valid) {
       const { email, password } = this.form.value;
 
       this.store.dispatch(UserActions.login({ email, password }));
     }
+  }
+
+  logout(){
+    this.store.dispatch(UserActions.logout())
   }
 }
