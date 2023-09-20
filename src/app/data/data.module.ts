a31c8051c932a39dfd5cb1d1a86610dead2d0f9d
FelipeCollantes <felipe.pcollantes@gmail.com>
2023-09-20 14:56:00 +0200

    Add usecases

--- src/app/data/data.module.ts
+++ src/app/data/data.module.ts
@@ -4,6 +4,13 @@ import { ProductRepository } from '../domain/product/repositories/product.reposi
 import { GetProductsUseCase } from '../domain/product/usecases/get-products.usecase';
 import { ProductImplementationRepository } from './repositories/product/product-implementation.repository';
 import { HttpClientModule } from '@angular/common/http';
+import { GetProductDetailsUseCase } from '../domain/product/usecases/get-product-details';
+import { UserRepository } from '../domain/user/repositories/user.repository';
+import { UserLoginUseCase } from '../domain/user/usecases/user-login.usecase';
+import { UserLogoutUseCase } from '../domain/user/usecases/user-logout.usecase';
+import { UserRegisterUseCase } from '../domain/user/usecases/user-register.usecase';
+import { UserImplementationRepository } from './repositories/user/user-implementation.repository';
+import { GetUserProfileUseCase } from '../domain/user/usecases/get-user-profile.usecase';
 
 const getProductsUseCaseFactory = (productRepo: ProductRepository) => new GetProductsUseCase(productRepo);
 export const getProductsUseCaseProvider = {
@@ -12,8 +19,52 @@ export const getProductsUseCaseProvider = {
   deps: [ProductRepository],
 };
 
+const getProductDetailUseCaseFactory = (productRepo: ProductRepository) => new GetProductDetailsUseCase(productRepo);
+export const getProductDetailUseCaseProvider = {
+  provide: GetProductDetailsUseCase,
+  useFactory: getProductDetailUseCaseFactory,
+  deps: [ProductRepository],
+};
+
+const getUserLoginUseCaseFactory = (userRepo: UserRepository) => new UserLoginUseCase(userRepo);
+export const getUserLoginUseCaseProvider = {
+  provide: UserLoginUseCase,
+  useFactory: getUserLoginUseCaseFactory,
+  deps: [UserRepository],
+};
+
+const getUserLogoutUseCaseFactory = (userRepo: UserRepository) => new UserLogoutUseCase(userRepo);
+export const getUserLogoutUseCaseProvider = {
+  provide: UserLogoutUseCase,
+  useFactory: getUserLogoutUseCaseFactory,
+  deps: [UserRepository],
+};
+
+const registerUserUseCaseFactory = (userRepo: UserRepository) => new UserRegisterUseCase(userRepo);
+export const registerUserUseCaseProvider = {
+  provide: UserRegisterUseCase,
+  useFactory: registerUserUseCaseFactory,
+  deps: [UserRepository],
+};
+
+const getProfileUserUseCaseFactory = (userRepo: UserRepository) => new GetUserProfileUseCase(userRepo);
+export const getProfileUserUseCaseProvider = {
+  provide: GetUserProfileUseCase,
+  useFactory: getProfileUserUseCaseFactory,
+  deps: [UserRepository],
+};
+
 @NgModule({
-  providers: [getProductsUseCaseProvider, { provide: ProductRepository, useClass: ProductImplementationRepository }],
+  providers: [
+    getProductsUseCaseProvider,
+    getProductDetailUseCaseProvider,
+    getUserLoginUseCaseProvider,
+    getUserLogoutUseCaseProvider,
+    registerUserUseCaseProvider,
+    getProfileUserUseCaseProvider,
+    { provide: ProductRepository, useClass: ProductImplementationRepository },
+    { provide: UserRepository, useClass: UserImplementationRepository },
+  ],
   declarations: [],
   imports: [CommonModule, HttpClientModule],
 })
