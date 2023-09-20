0c5b6f8c30c43306b6d92bd0e5b5ace6a30c008b
Felipecollantes <felipe.pcollantes@gmail.com>
2023-09-20 09:28:45 +0200

    Implement menu

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import * as FromUser from '../../data/store/user/selectors';
+import { Store } from '@ngrx/store';
+import { RootState } from 'src/app/data/store';
 
 @Component({
   selector: 'custom-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  public user$ = this.store.select(FromUser.selectUser);
+  public isMenuOpen: boolean = false;
+
+  constructor(private store: Store<RootState>) {}
+}
