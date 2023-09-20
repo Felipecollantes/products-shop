28412a3dd0061039c0beddc079965e09c281adbe
FelipeCollantes <felipe.pcollantes@gmail.com>
2023-09-20 11:49:16 +0200

    refactor code

--- src/app/core/interceptors/set-headers.interceptor.ts
+++ src/app/core/interceptors/set-headers.interceptor.ts
@@ -1,24 +1,22 @@
 import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Store } from '@ngrx/store';
-import { Observable, catchError, takeUntil, throwError } from 'rxjs';
+import { Observable, catchError, throwError } from 'rxjs';
 import { RootState } from 'src/app/data/store';
 import * as FromUser from '../../data/store/user/selectors';
 import { Injectable } from '@angular/core';
-import { HttpCancelService } from '../services/cancel/http-cancel.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SetHeaderInterceptor implements HttpInterceptor {
   private accessToken: string | undefined = '';
-  constructor(private store: Store<RootState>, private httpCancelService: HttpCancelService) {
+  constructor(private store: Store<RootState>) {
     this.store.select(FromUser.selectToken).subscribe((accessToken) => (this.accessToken = accessToken?.access_token));
   }
 
   intercept<T>(req: HttpRequest<T>, next: HttpHandler): Observable<HttpEvent<any>> {
     if (this.accessToken) req = req.clone({ setHeaders: { authorization: `Bearer ${this.accessToken}` } });
     return next.handle(req).pipe(
-      takeUntil(this.httpCancelService.onCancelPendingRequests()),
       catchError((error: HttpErrorResponse) => {
         if (error.status >= 500) console.error(error);
         if (error.status === 401) console.error('Unauthorized ', error);
