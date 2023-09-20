5a959583af7b460b6d8ccdf468a42ca4abc266cc
Felipecollantes <felipe.pcollantes@gmail.com>
2023-09-20 10:46:21 +0200

    Create auth guard

--- src/app/core/guards/auth/auth.guard.ts
+++ src/app/core/guards/auth/auth.guard.ts
@@ -1,50 +1,15 @@
-import { Injectable } from '@angular/core';
-import { CanActivateFn, CanLoad, CanMatchFn } from '@angular/router';
-import { Store } from '@ngrx/store';
-import { Observable, map } from 'rxjs';
-import { RootState } from 'src/app/data/store';
-import * as FromUser from '../../../data/store/user/selectors';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../../services/auth/auth.service';
+import { PATHS } from '../../constants/path.const';
+import { inject } from '@angular/core';
 
-// @Injectable({ providedIn: 'root' })
-// export class AuthV3Guard {
-//   constructor(private store: Store<RootState>) {}
-
-//   canMatchTeam: CanActivateFn = () => {
-//     return true;
-//   };
-
-//   //   canMatchTeam: CanActivateFn = () => {
-//   //     return this.store.select(FromUser.selectToken).pipe(
-//   //         map(accessToken => {
-//   //           if (accessToken?.access_token) return true;
-//   //         //   this.routing.goRoot([SharedRoutes.introduction], { animated: true });
-//   //           return false;
-//   //         }),
-//   //       );
-//   //   };
-// }
-
-// export const canMatchTeam: CanMatchFn = () => {
-//   return false;
-// };
-
-export const AuthGuard: CanActivateFn = (): // route: ActivatedRouteSnapshot,
-// state: RouterStateSnapshot
-boolean => {
-  // const router: Router = inject(Router);
-  // const tokenStorage: TokenStorageService = inject(TokenStorageService);
-
-  // if (tokenStorage.isTokenExpired()) {
-  //   return router.navigate(['forbidden']);
-  // }
-  // else {
-  //   const roles = route.data['permittedRoles'] as Array<string>;
-  //   const userRole = tokenStorage.getUserToken().role;
-
-  //   if (roles && !roles.includes(userRole)) {
-  //     return router.navigate(['login']);
-  //   }
-  //   else
+export const AuthGuard: CanActivateFn = (): boolean => {
+  console.log('Auth guard');
+  const authService: AuthService = inject(AuthService);
+  const router: Router = inject(Router);
+  if (!authService.isAuthenticated()) {
+    router.navigate([`/${PATHS.home}`]);
+    return false;
+  }
   return true;
-  // }
 };
